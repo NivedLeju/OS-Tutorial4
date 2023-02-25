253e4d5bb982d3f0b6c73194472e2fb7ccf7c76d
DarrenConsOnTech <darren.constantine@ontariotechu.net>
2023-02-24 23:53:16 -0500

    Update players.h

--- jeopardy_source/players.h
+++ jeopardy_source/players.h
@@ -1,9 +1,9 @@
 /*
- * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
- * All rights reserved.
- *
+ * Tutorial 4 Jeopardy Project for SOFE 3950U: Operating Systems
+ * <Group Member:>
+ * Darren E. Constantine - 100782701
+ * Abdul Shahid - 100709858
+ * Nived Leju Ramachandran Sonia - 100782317
  */
 #ifndef PLAYERS_H_
 #define PLAYERS_H_
@@ -14,15 +14,17 @@
 
 // Player struct for each player
 typedef struct {
-    char* name[MAX_LEN];
-    int score;
+  char *name[MAX_LEN];
+  int score;
 } player;
 
 // Returns true if the player name matches one of the existing players
 extern bool player_exists(player *players, int num_players, char *name);
 
-// Go through the list of players and update the score for the 
+// Go through the list of players and update the score for the
 // player given their name
-extern void update_score(player *players, int num_players, char *name, int score);
-
+extern void update_score(player *players, int num_players,
+                         int score);
+// Displays standings
+extern void print_standings(player *players, int num_players);
 #endif /* PLAYERS_H_ */
